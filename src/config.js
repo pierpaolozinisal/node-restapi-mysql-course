719fe3a89f6c3e76f314ab4c774ee41ba4919c9b
pierpaolozinisal <pierpaolozini@outlook.es>
2022-10-07 22:40:30 +0100

    modifica config

--- src/config.js
+++ src/config.js
@@ -6,5 +6,5 @@ export const PORT = process.env.PORT || 3000
 export const DB_host= process.env.DB_HOST || "localhost"
 export const DB_port = process.env.DB_PORT || 3306
 export const DB_user =process.env.DB_USER || "root"
-export const DB_password = process.env.DB_PASSWORD  || ""
+export const DB_password = process.env.DB_PASSWORD  || "root"
 export const DB_database =process.env.DB_DATABASE || "test"
